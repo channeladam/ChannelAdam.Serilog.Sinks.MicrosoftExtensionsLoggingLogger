422566cccd38f9904e9e80cccb19a79ab4bfe5d7
AdamMCraven <adam@ChannelAdam.com>
2017-05-02 21:50:39 +1000

    Initial commit

---
+++ src/ChannelAdam.Serilog.Sinks.MicrosoftExtensionsLoggingLogger/MicrosoftExtensionsLoggingLoggerConfigurationExtensions.cs
@@ -0,0 +1,129 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MicrosoftExtensionsLoggingLoggerConfigurationExtensions.cs">
+//     Copyright (c) 2017 Adam Craven. All rights reserved.
+// </copyright>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------
+
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Display;
+using System;
+
+namespace ChannelAdam.Serilog.Sinks.MicrosoftExtensionsLoggingLogger
+{
+    public static class MicrosoftExtensionsLoggingLoggerConfigurationExtensions
+    {
+        #region Private Fields
+
+        private const string DefaultMessageFormatTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}";
+
+        #endregion Private Fields
+
+        #region Public Methods
+
+        /// <summary>
+        /// Writes log events to <see cref="Microsoft.Extensions.Logging.ILogger"/>.
+        /// </summary>
+        /// <param name="sinkConfiguration">Logger sink configuration.</param>
+        /// <param name="frameworkLogger">The Microsoft.Extensions.Logging.ILogger to use as a sink.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        public static LoggerConfiguration MicrosoftExtensionsLoggingLogger(
+            this LoggerSinkConfiguration sinkConfiguration,
+            Microsoft.Extensions.Logging.ILogger frameworkLogger)
+        {
+            return MicrosoftExtensionsLoggingLogger(sinkConfiguration, LevelAlias.Minimum, frameworkLogger, DefaultMessageFormatTemplate, null, null);
+        }
+
+        /// <summary>
+        /// Writes log events to <see cref="Microsoft.Extensions.Logging.ILogger"/>.
+        /// </summary>
+        /// <param name="sinkConfiguration">Logger sink configuration.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum level for events passed through the sink.</param>
+        /// <param name="frameworkLogger">The Microsoft.Extensions.Logging.ILogger to use as a sink.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        public static LoggerConfiguration MicrosoftExtensionsLoggingLogger(
+            this LoggerSinkConfiguration sinkConfiguration,
+            LogEventLevel restrictedToMinimumLevel,
+            Microsoft.Extensions.Logging.ILogger frameworkLogger)
+        {
+            return MicrosoftExtensionsLoggingLogger(sinkConfiguration, restrictedToMinimumLevel, frameworkLogger, DefaultMessageFormatTemplate, null, null);
+        }
+
+        /// <summary>
+        /// Writes log events to <see cref="Microsoft.Extensions.Logging.ILogger"/>.
+        /// </summary>
+        /// <param name="sinkConfiguration">Logger sink configuration.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum level for events passed through the sink.</param>
+        /// <param name="frameworkLogger">The Microsoft.Extensions.Logging.ILogger to use as a sink.</param>
+        /// <param name="messageFormatTemplate">A message template describing the format used to write to the sink. The default is "{Timestamp} [{Level}] {Message}{NewLine}{Exception}".</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        public static LoggerConfiguration MicrosoftExtensionsLoggingLogger(
+            this LoggerSinkConfiguration sinkConfiguration,
+            LogEventLevel restrictedToMinimumLevel,
+            Microsoft.Extensions.Logging.ILogger frameworkLogger,
+            string messageFormatTemplate)
+        {
+            return MicrosoftExtensionsLoggingLogger(sinkConfiguration, restrictedToMinimumLevel, frameworkLogger, messageFormatTemplate, null, null);
+        }
+
+        /// <summary>
+        /// Writes log events to <see cref="Microsoft.Extensions.Logging.ILogger"/>.
+        /// </summary>
+        /// <param name="sinkConfiguration">Logger sink configuration.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum level for events passed through the sink.</param>
+        /// <param name="frameworkLogger">The Microsoft.Extensions.Logging.ILogger to use as a sink.</param>
+        /// <param name="messageFormatTemplate">A message template describing the format used to write to the sink. The default is "{Timestamp} [{Level}] {Message}{NewLine}{Exception}".</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        public static LoggerConfiguration MicrosoftExtensionsLoggingLogger(
+            this LoggerSinkConfiguration sinkConfiguration,
+            LogEventLevel restrictedToMinimumLevel,
+            Microsoft.Extensions.Logging.ILogger frameworkLogger,
+            string messageFormatTemplate,
+            IFormatProvider formatProvider)
+        {
+            return MicrosoftExtensionsLoggingLogger(sinkConfiguration, restrictedToMinimumLevel, frameworkLogger, messageFormatTemplate, formatProvider, null);
+        }
+
+        /// <summary>
+        /// Writes log events to <see cref="Microsoft.Extensions.Logging.ILogger"/>.
+        /// </summary>
+        /// <param name="sinkConfiguration">Logger sink configuration.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum level for events passed through the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
+        /// <param name="frameworkLogger">The Microsoft.Extensions.Logging.ILogger to use as a sink.</param>
+        /// <param name="messageFormatTemplate">A message template describing the format used to write to the sink. The default is "{Timestamp} [{Level}] {Message}{NewLine}{Exception}".</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <param name="levelSwitch">A switch allowing the pass-through minimum level to be changed at runtime.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        public static LoggerConfiguration MicrosoftExtensionsLoggingLogger(
+            this LoggerSinkConfiguration sinkConfiguration,
+            LogEventLevel restrictedToMinimumLevel,
+            Microsoft.Extensions.Logging.ILogger frameworkLogger,
+            string messageFormatTemplate,
+            IFormatProvider formatProvider,
+            LoggingLevelSwitch levelSwitch)
+        {
+            if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
+            if (frameworkLogger == null) throw new ArgumentNullException(nameof(frameworkLogger));
+            if (messageFormatTemplate == null) throw new ArgumentNullException(nameof(messageFormatTemplate));
+
+            var formatter = new MessageTemplateTextFormatter(messageFormatTemplate, formatProvider);
+            return sinkConfiguration.Sink(new MicrosoftExtensionsLoggingLoggerSink(frameworkLogger, formatter), restrictedToMinimumLevel, levelSwitch);
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ src/ChannelAdam.Serilog.Sinks.MicrosoftExtensionsLoggingLogger/MicrosoftExtensionsLoggingLoggerSink.cs
@@ -0,0 +1,224 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MicrosoftExtensionsLoggingLoggerSink.cs">
+//     Copyright (c) 2017 Adam Craven. All rights reserved.
+// </copyright>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------
+
+using Microsoft.Extensions.Logging;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using System;
+using System.IO;
+
+namespace ChannelAdam.Serilog.Sinks.MicrosoftExtensionsLoggingLogger
+{
+    public class MicrosoftExtensionsLoggingLoggerSink : ILogEventSink
+    {
+        #region Private Fields
+
+        private readonly ITextFormatter _formatter;
+        private readonly ILogger _frameworkLogger;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        /// <summary>
+        /// Construct a sink that writes to the <see cref="Microsoft.Extensions.Logging.ILogger"/>.
+        /// </summary>
+        /// <param name="frameworkLogger">The Microsoft.Extensions.Logging.ILogger to use in this sink.</param>
+        /// <param name="formatter">Provides formatting for outputting log data</param>
+        public MicrosoftExtensionsLoggingLoggerSink(ILogger frameworkLogger, ITextFormatter formatter)
+        {
+            _frameworkLogger = frameworkLogger;
+            _formatter = formatter;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        /// <summary>
+        /// Returns a ushort hash code for this string (for example useful for autogenerating EventIDs for the Windows Event Log based on the message string)
+        /// </summary>
+        /// <param name="s">the string</param>
+        /// <returns>A ushort hash code</returns>
+        /// <remarks>Credit to https://github.com/it2media/String/blob/master/src/IT2media.Extensions.String/StringExtensions.cs</remarks>
+        public static ushort GetHashCodeUShort(string s)
+        {
+            return (ushort)GetHashCodeInternal(s);
+        }
+
+        /// <summary>
+        /// Emit the provided log event to the sink.
+        /// </summary>
+        /// <param name="logEvent">The log event to write.</param>
+        public void Emit(LogEvent logEvent)
+        {
+            using (var sw = new StringWriter())
+            {
+                _formatter.Format(logEvent, sw);
+                var message = sw.ToString();
+
+                var exception = logEvent.Exception;
+
+                switch (logEvent.Level)
+                {
+                    case LogEventLevel.Verbose:
+                        if (exception == null)
+                        {
+                            _frameworkLogger.LogTrace(message);
+                        }
+                        else
+                        {
+                            _frameworkLogger.LogTrace(GetHashCode(message, exception, 0), exception, message);
+                        }
+                        break;
+
+                    case LogEventLevel.Debug:
+                        if (exception == null)
+                        {
+                            _frameworkLogger.LogDebug(message);
+                        }
+                        else
+                        {
+                            _frameworkLogger.LogDebug(GetHashCode(message, exception, 0), exception, message);
+                        }
+                        break;
+
+                    case LogEventLevel.Warning:
+                        if (exception == null)
+                        {
+                            _frameworkLogger.LogWarning(message);
+                        }
+                        else
+                        {
+                            _frameworkLogger.LogWarning(GetHashCode(message, exception, 0), exception, message);
+                        }
+                        break;
+
+                    case LogEventLevel.Error:
+                        if (exception == null)
+                        {
+                            _frameworkLogger.LogError(message);
+                        }
+                        else
+                        {
+                            _frameworkLogger.LogError(GetHashCode(message, exception, 0), exception, message);
+                        }
+                        break;
+
+                    case LogEventLevel.Fatal:
+                        if (exception == null)
+                        {
+                            _frameworkLogger.LogCritical(message);
+                        }
+                        else
+                        {
+                            _frameworkLogger.LogCritical(GetHashCode(message, exception, 0), exception, message);
+                        }
+                        break;
+
+                    default: // LogEventLevel.Information or other
+                        if (exception == null)
+                        {
+                            _frameworkLogger.LogInformation(message);
+                        }
+                        else
+                        {
+                            _frameworkLogger.LogInformation(GetHashCode(message, exception, 0), exception, message);
+                        }
+                        break;
+                }
+            }
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        /// <summary>
+        /// Returns an ushort hash based on the message, if message is null the exception's message is taken
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="exception"></param>
+        /// <param name="defaultHashCode"></param>
+        /// <returns></returns>
+        /// <remarks>Credit to https://github.com/it2media/Logging/blob/master/src/IT2media.Extensions.Logging.Abstractions/LoggerExtensions.cs</remarks>
+        private static int GetHashCode(string message, Exception exception, int defaultHashCode)
+        {
+            if (message == null)
+            {
+                return GetHashCodeFromException(exception, defaultHashCode);
+            }
+            else
+            {
+                return GetHashCodeUShort(message);
+            }
+        }
+
+        /// <summary>
+        /// Returns an ushort hash based on the exception's message
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <param name="defaultHashCode"></param>
+        /// <returns></returns>
+        /// <remarks>Credit to https://github.com/it2media/Logging/blob/master/src/IT2media.Extensions.Logging.Abstractions/LoggerExtensions.cs</remarks>
+        private static int GetHashCodeFromException(Exception exception, int defaultHashCode)
+        {
+            if (exception?.Message != null)
+            {
+                return GetHashCodeUShort(exception.Message);
+            }
+
+            return defaultHashCode;
+        }
+
+        private static int GetHashCodeInternal(string s)
+        {
+            // This hash code is a simplified version of the GetHashCode() used in .NET CoreFX Release 2.0.0
+            // https://github.com/dotnet/corefx/blob/release/2.0.0/src/Common/src/System/Text/StringOrCharArray.cs
+            //
+            // This hash code is a simplified version of some of the code in String,
+            // when not using randomised hash codes.  We don't use string's GetHashCode
+            // because we need to be able to use the exact same algorithms on a char[].
+            // As such, this should not be used anywhere there are concerns around
+            // hash-based attacks that would require a better code.
+
+            int count = s.Length;
+
+            int hash1 = (5381 << 16) + 5381;
+            int hash2 = hash1;
+
+            for (int i = 0; i < count; ++i)
+            {
+                int c = s[i];
+                hash1 = unchecked((hash1 << 5) + hash1) ^ c;
+
+                if (++i >= count)
+                {
+                    break;
+                }
+
+                c = s[i];
+                hash2 = unchecked((hash2 << 5) + hash2) ^ c;
+            }
+
+            return unchecked(hash1 + (hash2 * 1566083941));
+        }
+
+        #endregion Private Methods
+    }
+}
